a6ada80b40b3d756996330cba79e56eac0072399
MWPX777 <mwpx777@gmail.com>
2021-02-21 07:33:48 -0600

    finished with 11.1 module

--- server.js
+++ server.js
@@ -49,7 +49,12 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
-}
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -60,7 +65,18 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      if(result){
+          res.json(result);
+      }else{
+          res.send(404);
+      }
+  });
+
 // listen for requests
+
 app.listen(PORT, () => {
-    console.log('API server now on port ${PORT}!');
-});
+    console.log(`API server now on port ${PORT}!`);
+  });
